1ffd014e3440e02bb64d1b1de2e4c6b4905c107f
Gabriel <gabrielcoutinho.pro@gmail.com>
2026-09-12 01:18:25 -0300

    fix: pnpm config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { cloudflare } from '@cloudflare/vite-plugin';
-import StylexRsPlugin from '@stylexswc/unplugin/vite';
-import { devtools } from '@tanstack/devtools-vite';
-import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import viteReact from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { cloudflare } from '@cloudflare/vite-plugin'
+import StylexRsPlugin from '@stylexswc/unplugin/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 const config = defineConfig({
 	resolve: { tsconfigPaths: true },
 	plugins: [
-		devtools(),
 		cloudflare({ viteEnvironment: { name: 'ssr' } }),
 		tanstackStart(),
 		viteReact(),
@@ -20,6 +18,6 @@ const config = defineConfig({
 			useCSSLayers: true,
 		}),
 	],
-});
+})
 
-export default config;
+export default config
